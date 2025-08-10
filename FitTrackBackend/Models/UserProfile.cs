829892d86aeb411047154a8c897411ae8ad850a5
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-08-10 12:53:55 +0400

    Add UserProfile.cs

---
+++ FitTrackBackend/Models/UserProfile.cs
@@ -0,0 +1,7 @@
+public class UserProfile {
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Goals { get; set; }
+    public string Preferences { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
